08dfd4c4ee51d593dff2c792d628ba8aa454ee77
Keheliya-Sathsara <keheliyasm@gmail.com>
2023-10-04 10:48:51 +0530

    profile image scale changing

--- lib/sections/topSection/components/person_pic.dart
+++ lib/sections/topSection/components/person_pic.dart
@@ -3,13 +3,20 @@ import 'package:flutter/material.dart';
 class PersonPic extends StatelessWidget {
   const PersonPic({
     Key? key,
+    this.scale = 1.5, // Adjust the scale factor as needed
   }) : super(key: key);
+
+  final double scale;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       constraints: const BoxConstraints(maxWidth: 639, maxHeight: 860),
-      child: Image.asset("assets/images/person.png"),
+      child: Transform.scale(
+        scale:
+            scale, // Set the scale factor here to increase or decrease the size
+        child: Image.asset("assets/images/person.png"),
+      ),
     );
   }
 }
